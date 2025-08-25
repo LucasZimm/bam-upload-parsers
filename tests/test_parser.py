c9de44d1d93dd1c2268aed94bd5488df99c2b743
Lucas Zimmermann <lucas.zimmermann@bam.de>
2025-08-25 13:59:42 +0200

    rewrote test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,4 @@
+from bam_masterdata.datamodel.object_types import ExperimentalStep
 from bam_masterdata.logger import logger
 from bam_masterdata.metadata.entities import CollectionType
 
@@ -7,8 +8,10 @@ class TestMasterdataParserExample:
         collection = CollectionType()
         parser.parse([], collection, logger)
 
-        assert len(collection.attached_objects) == 2
-        objects = list(collection.attached_objects.values())
-        assert objects[0].name == "Synthesis"
-        assert objects[1].name == "Measurement"
+        assert len(collection.attached_objects) == 0
+        id1 = collection.add(ExperimentalStep(name="test"))
+        parser.parse([], collection, logger)
+        assert len(collection.attached_objects) == 0
+        id2 = collection.add(ExperimentalStep(name="test2"))
+        collection.add_relationship(id1, id2)
         assert len(collection.relationships) == 1
