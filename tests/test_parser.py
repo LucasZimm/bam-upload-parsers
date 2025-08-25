3cdbed48ae9086a7601a9b4589fdae7c2df11974
Lucas Zimmermann <lucas.zimmermann@bam.de>
2025-08-25 14:03:27 +0200

    fix

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -11,7 +11,7 @@ class TestMasterdataParserExample:
         assert len(collection.attached_objects) == 0
         id1 = collection.add(ExperimentalStep(name="test"))
         parser.parse([], collection, logger)
-        assert len(collection.attached_objects) == 0
+        assert len(collection.attached_objects) == 1
         id2 = collection.add(ExperimentalStep(name="test2"))
         collection.add_relationship(id1, id2)
         assert len(collection.relationships) == 1
